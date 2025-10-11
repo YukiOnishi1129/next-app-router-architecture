808ed489ce5065480cba68ab7827be84be3f5948
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-10-12 03:17:02 +0900

    fix

--- frontend/src/features/requests/actions/createRequest.action.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { createRequestServer } from '@/external/handler/request/command.server'
-
-import type { CreateRequestInput } from '@/features/requests/schemas'
-
-export async function createRequestAction(input: CreateRequestInput) {
-  return createRequestServer(input)
-}
--- frontend/src/features/requests/actions/reopenRequest.action.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { reopenRequestServer } from '@/external/handler/request/command.server'
-
-import type { ReopenRequestInput } from '@/external/dto/request'
-
-export async function reopenRequestAction(input: ReopenRequestInput) {
-  return reopenRequestServer(input)
-}
